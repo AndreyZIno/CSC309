1154ad0884714c36a6b3f77ab55ea70b88acb211
Elizabeth <eilina4162@gmail.com>
2024-10-20 15:11:07 -0400

    Implemented retrieving template with id

---
+++ pages/api/templates/[id].js
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+    if (req.method !== 'GET') {
+        return res.status(405).json({ message: 'Method not allowed' });
+    }
+
+    try {
+        const { id } = req.query;
+        const template = await prisma.template.findUnique({
+            where: { id: parseInt(id) },
+        });
+
+        if (!template) {
+            return res.status(404).json({ message: 'Cannot find template' });
+        }
+
+        res.status(200).json(template);
+
+    }catch(error) {
+        console.error('Error fetching template:', error);
+        res.status(500).json({ error: 'Cannot fetch template' });
+    }
+}
