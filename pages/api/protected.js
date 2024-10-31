d6e5e062a39f16f878ddf941582f2b7709b9a234
Elizabeth <eilina4162@gmail.com>
2024-10-30 20:16:51 -0400

    Generate model, fix some bugs, add a check to create blogs

--- pages/api/protected.js
+++
@@ -1,7 +0,0 @@
-import { authenticate } from '../../lib/authentication';
-
-function handler(req, res) {
-    res.status(200).json({ message: 'In protected route', user: req.user} );
-}
-
-export default authenticate(handler);
