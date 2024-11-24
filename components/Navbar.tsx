4718319366fd00bb817da5e9866a7419e40b1126
Elizabeth <eilina4162@gmail.com>
2024-11-24 18:25:19 -0500

    fix dark mode button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import ThemeToggleButton from './ThemeToggleButton'; 
 
 interface NavbarProps {
   user: {
     avatar: string;
     firstName: string;
-    isAdmin: boolean; // Check if the user is an admin
+    isAdmin: boolean;
   };
   isGuest: boolean;
   onLogout: () => void;
@@ -25,26 +26,30 @@ const Navbar: React.FC<NavbarProps> = ({ user, isGuest, onLogout }) => {
   };
 
   return (
-    <nav className="bg-blue-600 text-white p-4 shadow">
+    <nav className="bg-blue-600 text-white p-4 shadow dark:bg-blue-900 dark:text-blue-200">
       <div className="container mx-auto flex justify-between items-center">
-        <h1
-          className="text-xl font-bold cursor-pointer"
+      <h1
+          className="text-xl font-bold cursor-pointer hover:text-purple-200 dark:hover:text-yellow-500"
           onClick={handleNavigateHome}
         >
           Scriptorium
         </h1>
         <ul className="flex space-x-6">
-          <li>
-            <Link href={isGuest ? "/templates/viewAll?guest=true" : "/templates/viewAll"}>
-              Templates
-            </Link>
+            <li>
+                <Link href={isGuest ? "/templates/viewAll?guest=true" : "/templates/viewAll"}
+                  className="hover:underline dark:hover:text-yellow-500"
+                >
+                Templates
+                </Link>
+            </li>
+            <li>
+                <Link href={isGuest ? "/blogs/viewAll?guest=true" : "/blogs/viewAll"}
+                  className="hover:underline dark:hover:text-yellow-500"
+                >
+                Blogs
+                </Link>
           </li>
-          <li>
-            <Link href={isGuest ? "/blogs/viewAll?guest=true" : "/blogs/viewAll"}>
-              Blogs
-            </Link>
-          </li>
-          {user.isAdmin && ( // Only render if the user is an admin
+          {user.isAdmin && (
             <li>
               <Link href="/admin/reports">
                 Reports
@@ -52,6 +57,9 @@ const Navbar: React.FC<NavbarProps> = ({ user, isGuest, onLogout }) => {
             </li>
           )}
         </ul>
+
+        <ThemeToggleButton />
+        
         {!isGuest ? (
           <div className="relative">
             <img
@@ -61,22 +69,16 @@ const Navbar: React.FC<NavbarProps> = ({ user, isGuest, onLogout }) => {
               onClick={() => setMenuOpen(!menuOpen)}
             />
             {menuOpen && (
-              <div className="absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg">
+              <div className="absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg dark:bg-gray-700 dark:border-gray-600">
                 <ul>
                   <li
-                    className="px-4 py-2 text-black hover:bg-gray-100 cursor-pointer"
-                    onClick={() => alert('Dark Mode Toggle Coming Soon!')}
-                  >
-                    Dark Mode
-                  </li>
-                  <li
-                    className="px-4 py-2 text-black hover:bg-gray-100 cursor-pointer"
+                    className="px-4 py-2 text-black hover:bg-gray-100 cursor-pointer dark:text-gray-200 dark:hover:bg-gray-600"
                     onClick={() => router.push('/profile')}
                   >
                     Edit Profile
                   </li>
                   <li
-                    className="px-4 py-2 text-black hover:bg-gray-100 cursor-pointer"
+                    className="px-4 py-2 text-black hover:bg-gray-100 cursor-pointer dark:text-gray-200 dark:hover:bg-gray-600"
                     onClick={onLogout}
                   >
                     Logout
@@ -88,7 +90,7 @@ const Navbar: React.FC<NavbarProps> = ({ user, isGuest, onLogout }) => {
         ) : (
           <button
             onClick={onLogout}
-            className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
+            className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 dark:bg-red-400 dark:hover:bg-red-500"
           >
             Exit
           </button>
